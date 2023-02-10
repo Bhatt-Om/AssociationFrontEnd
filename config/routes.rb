46d9f5bb96faf8995969355515921fd1f456b071
Bhatt-Om <122429893+Bhatt-Om@users.noreply.github.com>
2023-02-10 12:57:30 +0530

    Complite the front end with has_many association

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "home#index"
+  root "articles#index"
+  resources :articles
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_09_125637) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_10_060634) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
   create_table "device_warrenties", force: :cascade do |t|
     t.string "duration"
     t.datetime "created_at", null: false
@@ -37,5 +46,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_09_125637) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "users"
   add_foreign_key "devices", "device_warrenties"
 end
